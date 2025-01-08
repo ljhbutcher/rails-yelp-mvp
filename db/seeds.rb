c25d9473bd55247ba58c99616569faf64d8929e3
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-08 12:03:12 +0000

    yelp models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create(name: "Epicure", address: "75008 Paris", phone_number: "1234567890", category: "french")
+Restaurant.create(name: "Sushi Samba", address: "110 Bishopsgate, London", phone_number: "0987654321", category: "japanese")
+Restaurant.create(name: "Pasta Palace", address: "123 Pasta Lane, Rome", phone_number: "1122334455", category: "italian")
+Restaurant.create(name: "Waffle House", address: "456 Waffle St, Brussels", phone_number: "2233445566", category: "belgian")
+Restaurant.create(name: "Dragon's Den", address: "789 Dragon Rd, Beijing", phone_number: "3344556677", category: "chinese")
